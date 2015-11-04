896dd5721d3752fc6cd7205caed7eae6727c5b0b
mayankaryanith <mayankaryanith@gmail.com>
2015-11-04 19:12:33 +0100

    [scull]chrater devices The commit contains TLB address translation as well as page table entry information and scull init and scull exit

---
+++ linux_developement/scull.c
@@ -0,0 +1,49 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include "scull.h"
+
+static int __init scull_init(void)
+{
+    int i,res = alloc_chrdev_region(dev,0,
+                SCULL_DEVICE_NO, SCULL_NAME);
+    if(res)
+    {
+        printk(KERN_ALERT "failed to allocated scull structure");
+        return res;
+    }
+    else
+    {
+        printk(KERN_ALERT "succesful allocation of scull structure /n %d",MAJOR(*dev));
+    }
+    scull_devices=kmalloc(SCULL_DEVICE_NO*sizeof(scull_devices),GFP_KERNEL);
+    if(!scull_devices)
+    {
+       return -ENOMEM;
+    }
+    else
+	{
+	memset(scull_devices,0,SCULL_DEVICE_NO*sizeof(scull_devices));//did not see why to memset all the values to zero but assuming it to be something
+	i=0;
+	while(i< SCULL_DEVICE_NO)
+	{
+	scull_devices->quantum=SCULL_QUANTUM_BYTES;
+	scull_devices->qset=SCULL_QSET;
+	scull_devices++;	
+	i++;
+	}
+	return 1;//Bad patch
+	}
+}
+static void __exit scull_cleanup(void)
+{
+	unregister_chrdev_region(*dev,SCULL_DEVICE_NO);
+  	kfree(scull_devices);
+	printk(KERN_ALERT "goodbye! scull");
+}
+
+module_init(scull_init);
+module_exit(scull_cleanup);
+MODULE_LICENSE("GPL");
---
+++ linux_developement/scull.h
@@ -0,0 +1,29 @@
+#ifndef __SCULL_H__
+#define __SCULL_H__
+#include <linux/kdev_t.h>
+#include <linux/cdev.h>
+#include <linux/semaphore.h>
+#include <linux/fs.h>
+#define SCULL_DEVICE_NO 4
+#define SCULL_PIPE_NO   4
+#define SCULL_QUANTUM_BYTES 4000
+#define SCULL_QSET          1000
+#define SCULL_NAME          "scull"
+struct scull_qset
+{
+    void **data;
+    struct scull_qset *next;
+};
+struct scull_dev
+{
+    struct scull_qset *data;
+    int quantum;
+    int qset;
+    unsigned long size;
+    unsigned int access_key;
+    struct semaphore sem;
+    struct cdev cdev;
+};
+dev_t *dev;
+struct scull_dev *scull_devices;
+#endif
