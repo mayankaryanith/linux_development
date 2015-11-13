50a8f9934f69d71031365f9c2ad95d26df8f75bf
Neo <you@example.com>
2015-11-13 23:21:53 +0100

    Added scull trim and scull open

--- linux_developement/scull.h
+++ linux_developement/scull.h
@@ -29,5 +29,6 @@ dev_t *dev;
 struct scull_dev *scull_devices;
 ssize_t scull_read (struct file *, char __user *, size_t, loff_t *);
 ssize_t scull_write(struct file *, const char __user *, size_t, loff_t *);
- int scull_release(struct inode *, struct file *);
+int scull_release(struct inode *, struct file *);
+int scull_open(struct inode *, struct file *);
 #endif
