50de1d5ae3faf37a752eb0bffb906942675a80f7
Neo <you@example.com>
2016-02-25 20:50:38 +0100

    fun_editor stable version with backspace left right and insert mode wordcount
    Activity: fun_editor

--- fun_editor.c
+++ fun_editor.c
@@ -60,16 +60,19 @@ KEY_MESSAGE	Message key*/
     #include<stdlib.h>
     #include<signal.h>
     #include<string.h>
+    #include<sys/types.h>
     #include<unistd.h>
     #define KEY_ESC '\033'
     bool overtype_mode=false;
-
+    fpos_t pos;
     int main(int argc, char *argv[])
     {
-
-    int c,x,y,size=1,seek=0,word_count,line_count;
-    int *buff,*buff_beg,*buff_end,**buff_curr,*temp_buff;
+    int c,x,y;
     FILE *fp;
+    long lSize;
+    long Fin;
+    long position;
+    char * buffer;
         // If file name is not given close the program
         if(argc!=2)
         {
@@ -86,44 +89,27 @@ KEY_MESSAGE	Message key*/
     	    	noecho();                   // echo mode OFF
 		        cbreak();                   //Line buffering disabled, every character enabled
     	    	scrollok(stdscr, TRUE);     // scrolling mode ON
-                printw("The editor is up and running press ESC to exit.\n");
 		        refresh();
-                fp=fopen(argv[1],"ab+");
-        if(!fp)
+                fp=fopen(argv[1],"r+");
+        if(!fp)//if the file does not exist, create it
         {
+            fp=fopen(argv[1],"w+");
+            if(fp==NULL)
+            {
             printw("Error opening file");
             refresh();
-		    endwin();
             exit(1);
+		    endwin();
+            }
         }
         else
         {
-           buff_beg=buff_end=NULL;
-           buff=malloc(size*sizeof(int));
-           buff_curr=buff;
-           if(!buff)
-            {
-                printw(" Malloc memory allocation failed");
-                refresh();
-                exit(1);
-            }
            //If the file exists read the file
            while((c=getc(fp))!=EOF)
            {
                if(c!='\n') //dont know why I added the check for new line character
                 {
                     addch(c);
-                    //temp buffer that holds the value read from the file
-                    buff[size-1]=c;
-                    buff_curr=&buff[size-1];
-                    size++;
-                    if(!(temp_buff=realloc(buff,size*sizeof(int))))
-                    {
-                        printw("Realloc cannot allocate the requested memory address");
-                        refresh();
-                        free(buff);
-                    }
-                    buff=temp_buff;
                 }
                else
                {
@@ -162,9 +148,9 @@ KEY_MESSAGE	Message key*/
 		        noecho();
 		        getyx(stdscr,y,x);
 		        move(y,0);
-                fseek(fp,0,SEEK_SET);
 		        cbreak();
 		        refresh();
+                fseek(fp,0,SEEK_SET);
                 break;
             case KEY_UP:
 		        nocbreak();
@@ -183,24 +169,23 @@ KEY_MESSAGE	Message key*/
                overtype_mode=false;
                 break;
             case KEY_LEFT:
+               refresh();
 		       nocbreak();
 		       noecho();
 		       getyx(stdscr,y,x);
 		       move(y,x-1);
-               fseek(fp,-1,SEEK_CUR);
-               buff_curr--;
 		       cbreak();
 		       refresh();
+               fseek(fp,-1,SEEK_CUR);
                break;
             case KEY_RIGHT:
                nocbreak();
 			   noecho();
 			   getyx(stdscr,y,x);
 			   move(y,x+1);
-               fseek(fp,1,SEEK_CUR);
-               buff_curr++;
 			   cbreak();
 		       refresh();
+               fseek(fp,+1,SEEK_CUR);
                break;
             case KEY_EOS:
                nocbreak();
@@ -219,27 +204,40 @@ KEY_MESSAGE	Message key*/
             	refresh();
  		        break;
             case KEY_NPAGE:
-            //Flush word_count and line_count everytime keynpage is pressed so as to get the current value
-                word_count=0;
-                line_count=0;
-            //for word count and line count, start from the begining till the EOF and count
-                fseek(fp,0,SEEK_SET);
-                while((c=getc(fp)!=EOF))
-                {
-                    word_count++;
-                    if(c=='\n')
-                    line_count++;
-                }
-                erase();
-                printw("wordcount: %d \t",word_count);
-                printw("linecount: %d",line_count);
-                refresh();
-           //could have added a goto Label but it can be fatal and lead to memory corruption and hence clear the screen and add the content on the screen again
-           //no need to update the buffers buff and size value though
-                fseek(fp,0,SEEK_SET);
                 getbegyx(stdscr,y,x);
                 move(y+1,x);
+                erase();
                 refresh();
+                printw("wordcount: %d \n",ftell(fp));
+                refresh();
+                fseek(fp,0,SEEK_SET);
+                while((c=getc(fp))!=EOF)
+                {
+                    if(c!='\n') //dont know why I added the check for new line character
+                    {
+                        addch(c);
+                    }
+                    else
+                    {
+                        getyx(stdscr,y,x);
+                        move(y+1,0);
+                        refresh();
+                    }
+
+                }
+                break;
+            case KEY_BACKSPACE:  //ftruncate the file by each character when back_space is pressed,fd is required beacause as ftruncate only works with open 
+		        nocbreak();
+		        noecho();
+		        getyx(stdscr,y,x);
+		        move(y,x-1);
+		        delch();
+		        cbreak();
+                ftruncate(fileno(fp),ftell(fp)-1);
+                getbegyx(stdscr,y,x);
+                move(y+1,x);
+                erase();
+                fseek(fp,0,SEEK_SET);
                 while((c=getc(fp))!=EOF)
                 {
                     if(c!='\n') //dont know why I added the check for new line character
@@ -256,64 +254,31 @@ KEY_MESSAGE	Message key*/
                 }
                 refresh();
                 break;
-            case KEY_BACKSPACE:  //This is done by overwriting the deleted character with a blank space
-		        nocbreak();
-		        noecho();
-		        getyx(stdscr,y,x);
-		        move(y,x-1);
-		        delch();
-                fseek(fp,-1,SEEK_CUR);
-                fputc('\0',fp);
-		        cbreak();
-		        refresh();
             default: // write to the file
-                if(!overtype_mode)  // overtype mode where you overwrite a character in existing location
+                if(overtype_mode)  // overtype mode where you overwrite a character in existing location
                 {
-                    //temp buffer that holds the value of the text entered through the keyboard
-                        buff[size-1]=c;
-                        buff_curr=&buff[size-1];
-                        size++;
-                    if(!(temp_buff=realloc(buff,size*sizeof(int))))
-                    {
-                        printw("Realloc cannot allocate the requested memory address");
-                        refresh();
-                        free(buff);
-                    }
-                    buff=temp_buff;
-                    //We change the file pointer to a position to the right to check if their is non \0 charater to the right
-                    fseek(fp,1,SEEK_CUR);
-                    if((c=fgetc(fp))!= EOF)
-                    {
-                        while(**buff_curr!='\0')
-                        {
-                            fputc(**buff_curr,fp);
-                            buff_curr++;
-                            seek++;
-                            size++;
-                    //Ncurses screen needs to be updated with dynamic shifting and printing of characters to the right
-                            getyx(stdscr,y,x);
-                            move(y,x+1);
-                            addch(**buff_curr);
-                        }
-                    //Restore to current position and set seek to 0 and bring back the current buff to previous location movecursor location on the screen where it was assuming it fits in one line :(
-                        buff_curr=buff_curr-seek;
-                        fseek(fp,-seek,SEEK_CUR);
-                        move(y,x-seek);
-                        refresh();
-                        seek=0;
-                    }
+                    fseek(fp, 0L, SEEK_END);
+                    Fin=ftell(fp);
+                    fseek(fp, position, SEEK_SET);//current "position" in the file
+                    lSize = Fin - position;//lenght from where you want to insert the chars and the eof
+                    buffer = (char*) malloc(sizeof(char) * lSize);
+                    fread(buffer, 1, lSize, fp);//fread (Fin - position) into buffer
+                    fseek(fp, position, SEEK_SET);//fseek to position
+                    fputc(c, fp);//write the character and it is a char pointer
+                    fputs(buffer, fp);//fwrite the buffer
                 }
                 getyx(stdscr,y,x);
                 addch(c);
-                fputc(mvinch(y,x-1) & A_CHARTEXT,fp);
+                refresh();
+                fputc(mvinch(y,x) & A_CHARTEXT,fp);
                 move(y,x+1);
              }
 	      }
 		  refresh();
 		  fclose(fp);
           fp=NULL;
-          free(buff);
-          buff=buff_curr=NULL;
+          free(buffer);
+          buffer=NULL;
 		  endwin();
 	}
         return 0;
